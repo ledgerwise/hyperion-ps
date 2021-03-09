38c6a024a4ad6e3293896a4b98c9fb86daa5f11e
Jorge Ferrando <jorfermo@gmail.com>
2021-03-09 09:38:54 +0100

    comment usage example

--- index.js
+++ index.js
@@ -157,26 +157,22 @@ class HPSC extends EventEmitter {
 
 module.exports = HPSC;
 
-const client = new HPSC({
-  endpoints: [
-    'https://api.waxsweden.org',
-    'https://api.wax.alohaeos.com',
-    'https://wax.pink.gg',
-  ],
-  fetchEndpointList: true,
-  chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-  startBlock: 4957000,
-  fetchDeltas: false,
-  account: 'koilamarkett',
-  name: null,
-});
+// const client = new HPSC({
+//   endpoints: ['https://testnet.wax.pink.gg', 'https://testnet.waxsweden.org'],
+//   fetchEndpointList: false,
+//   chainId: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
+//   startBlock: 0,
+//   fetchDeltas: false,
+//   account: 'certifydlwse',
+//   name: null,
+// });
 
-logger.info('Starting client');
-client.start();
-client.on('actions', (actions) => {
-  for (const action of actions) {
-    logger.debug(
-      `${action.block_num} ${action.global_sequence} ${action.timestamp} ${action.act.name}`
-    );
-  }
-});
+// logger.info('Starting client');
+// client.start();
+// client.on('actions', (actions) => {
+//   for (const action of actions) {
+//     logger.debug(
+//       `${action.block_num} ${action.global_sequence} ${action.timestamp} ${action.act.name}`
+//     );
+//   }
+// });
